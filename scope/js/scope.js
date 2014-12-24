d72f0e99673a69d9737261670c0ebef18a9764c0
ynCode <younixiao@qq.com>
2014-12-24 09:49:09 +0800

    change ng-app name

--- scope/js/scope.js
+++ scope/js/scope.js
@@ -1,6 +1,6 @@
-angular.module('scopeExample', [])
+angular.module('dateModel', [])
     .controller('MyController', ['$scope', function ($scope) {
-        $scope.username = 'World';
+        $scope.username = 'World'; // wirte "World" into input which ng-model = username before fill
 
         $scope.sayHello = function () {
             $scope.greeting = 'Hello ' + $scope.username + '!';
